03c8e1351fb7fb322d481128a8d03c99c376dc80
szczepix <szczepan.martyniuk@gmail.com>
2021-09-18 21:22:51 +0200

    auto-commit-1631992971360

--- web/source/_static/init_table.js
+++ web/source/_static/init_table.js
@@ -2,7 +2,7 @@ console.log("init_table");
 $(document).ready(function() {
     console.log("loaded_table");
     $('#my-table').DataTable( {
-        "ajax": 'https://raw.githubusercontent.com/szczepix1983/content/main/web/content.json?_=' + Date.now(),
+        "ajax": 'https://raw.githubusercontent.com/szczepix1983/content/main/web/products.json?_=' + Date.now(),
         "columns": [
             { "content" : "" }
         ]
